9fa6c780b1cf48a054f9d265e874201fc779de41
Carlos Gutierrez <ingecarlos.gutierrez@gmail.com>
2020-01-30 15:57:06 -0600

    terminando la comida

--- src/view/ruby2d.rb
+++ src/view/ruby2d.rb
@@ -12,7 +12,26 @@ module View
                 title: "Snake", 
                 width: @pixel_size * state.grid.cols, 
                 height: @pixel_size * state.grid.rows)
+            render_food(state)
+            render_snake(state)
             show
+        end
+
+        private
+
+        def render_food(state)
+            extend Ruby2D::DSL
+            food = state.food
+            Square.new(
+                x: food.col * @pixel_size,
+                y: food.row * @pixel_size,
+                size: @pixel_size,
+                color: 'yellow'
+            )
+        end
+
+        def render_snake(state)
+
         end
     end
 end
