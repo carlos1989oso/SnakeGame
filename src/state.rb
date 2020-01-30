4fdfc7feff29444fe7d39b4ee5b1d873c8b585a6
Carlos Gutierrez <ingecarlos.gutierrez@gmail.com>
2020-01-30 15:06:14 -0600

    Agregando modulo de los estados

---
+++ src/state.rb
@@ -0,0 +1,20 @@
+module Model 
+    class Coord < Struct.new(row, col)
+        
+    end
+
+    class Food < Coord
+        
+    end
+    class Snake < Struct.new(positions)
+    
+    end
+
+    class Grid < Struct.new(rows, cols)
+        
+    end
+
+    class State < Struct.new(Snake, Food, Grid)
+        
+    end
+end
