a38d879cb25cbb974e587a9603667faf004af898
Carlos Gutierrez <ingecarlos.gutierrez@gmail.com>
2020-01-30 15:38:36 -0600

    Agregando Grid a la ventana

--- src/state.rb
+++ src/state.rb
@@ -15,14 +15,17 @@ module Model
     end
 
     class State < Struct.new(:Snake, :Food, :Grid)
-        
+
     end
 
     def self.initial_state
         Model::State.new(
-            Model::Snake.new,
-            Model::Food.new(0),
-            Model::Grid.new
+            Model::Snake.new([
+                Model::Coord.new(1,1)
+                Model::Coord.new(0,1)
+                ]),
+            Model::Food.new(4,4),
+            Model::Grid.new(8,12)
         )
     end
 end
--- src/view/ruby2d.rb
+++ src/view/ruby2d.rb
@@ -3,12 +3,16 @@ require "ruby2d"
 module View
     class Ruby2dView
         def initialize
-            
+            @pixel_size = 50
         end
 
         def render(state)
             extend Ruby2D::DSL
-            set(title: "Snake", width: 600, height: 400)
+            set(
+                title: "Snake", 
+                width: @pixel_size * state.grid.cols, 
+                height: @pixel_size * state.grid.rows
+            )
             show
         end
     end
